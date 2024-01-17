a9efbe9de4007b9be21a96ca67b95b6e94825477
Liab0606 <mrlab@126.com>
2024-01-17 08:53:32 +0000

    Project code and data

---
+++ source project/Main/Interface/AboutUs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Main.Interface
+{
+    public partial class AboutUs : Form
+    {
+        public AboutUs()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkLabel1.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Automatic detection of river capture based on planform pattern and χ-plot of the stream network");
+            MessageBox.Show("Title copied");
+        }
+    }
+}
